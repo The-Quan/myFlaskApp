e84277927890c7f6e6c5f498af5c5760e9ad93db
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-10-08 22:54:26 +0700

    up

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from post import get_posts, add_posts 
- # Import các hàm từ file posts
+from .post import get_posts, add_posts
+
 
 app = Flask(__name__)
 
